3e6327e5f591d0d12b478593013dfa2efcf92041
Fenil-S-Code <140561425+Fenil-S-Code@users.noreply.github.com>
2023-09-10 00:29:55 +0530

    responsive

---
+++ sigma.js
@@ -0,0 +1,22 @@
+// let obj = {
+//     title: "cashio",
+//     expertice: "clock",
+//     price: "thousand",
+//     max: function cashio(){
+//         console.log(this.price)
+
+//     }
+// }
+
+// obj.max();
+
+const baam = () => {
+    const newcolor = "blue"
+    btn.style.backgroundColor = newcolor
+}
+
+document.getElementById("btn").addEventListener(
+    "click",
+    baam
+)
+
